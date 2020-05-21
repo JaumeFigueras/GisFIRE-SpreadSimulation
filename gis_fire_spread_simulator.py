1545152cd5ae891173339c0246dcf879b7e1b742
JaumeFigueras <jaume.figueras@upc.edu>
2020-05-21 13:08:13 +0200

    Basic plugin ui: init plugin adds its menus and toolbars and unload pluguin removes its menus and toolbars. All taking into account if there exists other GisFIRE plugins loaded

--- gis_fire_spread_simulator.py
+++ gis_fire_spread_simulator.py
@@ -24,13 +24,15 @@
 from qgis.PyQt.QtCore import QSettings, QTranslator, QCoreApplication, Qt
 from qgis.PyQt.QtGui import QIcon
 from qgis.PyQt.QtWidgets import QAction
+from PyQt5.QtWidgets import QMenu
+from qgis.utils import active_plugins
+
 # Initialize Qt resources from file resources.py
 from .resources import *
 
 # Import the code for the DockWidget
 from .gis_fire_spread_simulator_dockwidget import GisFIRESpreadSimulatorDockWidget
 import os.path
-
 
 class GisFIRESpreadSimulator:
     """QGIS Plugin Implementation."""
@@ -61,18 +63,15 @@ class GisFIRESpreadSimulator:
             self.translator.load(locale_path)
             QCoreApplication.installTranslator(self.translator)
 
-        # Declare instance attributes
-        self.actions = []
-        self.menu = self.tr(u'&GisFIRE Wildfire Spread Simulator')
-        # TODO: We are going to let the user set this up in a future iteration
-        self.toolbar = self.iface.addToolBar(u'GisFIRESpreadSimulator')
-        self.toolbar.setObjectName(u'GisFIRESpreadSimulator')
-
-        #print "** INITIALIZING GisFIRESpreadSimulator"
-
-        self.pluginIsActive = False
-        self.dockwidget = None
-
+        # Initialization of UI references
+        self._toolbarActions = dict()
+        self._menuActions = dict()
+        self._menu = None
+        self._menu_gisfire = None
+        self._toolbar = None
+        self._dockwidget = None
+        # Initialization of GisFIRE data layers
+        self._layers = {}
 
     # noinspection PyMethodMayBeStatic
     def tr(self, message):
@@ -89,144 +88,81 @@ class GisFIRESpreadSimulator:
         # noinspection PyTypeChecker,PyArgumentList,PyCallByClass
         return QCoreApplication.translate('GisFIRESpreadSimulator', message)
 
+    def _addToolbarActions(self):
+        """Create the toolbar buttons that GisFIRE uses as shortcuts."""
+        pass
 
-    def add_action(
-        self,
-        icon_path,
-        text,
-        callback,
-        enabled_flag=True,
-        add_to_menu=True,
-        add_to_toolbar=True,
-        status_tip=None,
-        whats_this=None,
-        parent=None):
-        """Add a toolbar icon to the toolbar.
+    def _addMenuActions(self):
+        """Create the menu entries that allow GisFIRE procedures."""
+        pass
 
-        :param icon_path: Path to the icon for this action. Can be a resource
-            path (e.g. ':/plugins/foo/bar.png') or a normal file system path.
-        :type icon_path: str
-
-        :param text: Text that should be shown in menu items for this action.
-        :type text: str
-
-        :param callback: Function to be called when the action is triggered.
-        :type callback: function
-
-        :param enabled_flag: A flag indicating if the action should be enabled
-            by default. Defaults to True.
-        :type enabled_flag: bool
-
-        :param add_to_menu: Flag indicating whether the action should also
-            be added to the menu. Defaults to True.
-        :type add_to_menu: bool
-
-        :param add_to_toolbar: Flag indicating whether the action should also
-            be added to the toolbar. Defaults to True.
-        :type add_to_toolbar: bool
-
-        :param status_tip: Optional text to show in a popup when mouse pointer
-            hovers over the action.
-        :type status_tip: str
-
-        :param parent: Parent widget for the new action. Defaults None.
-        :type parent: QWidget
-
-        :param whats_this: Optional text to show in the status bar when the
-            mouse pointer hovers over the action.
-
-        :returns: The action that was created. Note that the action is also
-            added to self.actions list.
-        :rtype: QAction
-        """
-
-        icon = QIcon(icon_path)
-        action = QAction(icon, text, parent)
-        action.triggered.connect(callback)
-        action.setEnabled(enabled_flag)
-
-        if status_tip is not None:
-            action.setStatusTip(status_tip)
-
-        if whats_this is not None:
-            action.setWhatsThis(whats_this)
-
-        if add_to_toolbar:
-            self.toolbar.addAction(action)
-
-        if add_to_menu:
-            self.iface.addPluginToMenu(
-                self.menu,
-                action)
-
-        self.actions.append(action)
-
-        return action
-
+    def _addRelations(self):
+        """Create mutually exclusive relations between toolbar buttons."""
+        pass
 
     def initGui(self):
-        """Create the menu entries and toolbar icons inside the QGIS GUI."""
+        """Initializes the QGIS GUI for the GisFIRE Lightning plugin."""
+        # Setup the menu
+        menu_name = self.tr(u'Gis&FIRE')
+        parent_menu = self.iface.mainWindow().menuBar()
+        # Search if the menu exists (there are other GisFIRE modules installed)
+        for action in parent_menu.actions():
+            if action.text() == menu_name:
+                self._menu_gisfire = action.menu()
+        # Create the menu if does not exists and add it to the current menubar
+        if self._menu_gisfire is None:
+            self._menu_gisfire = QMenu(menu_name, self.iface.mainWindow().menuBar())
+            actions = self.iface.mainWindow().menuBar().actions()
+            self.iface.mainWindow().menuBar().insertMenu(actions[-1], self._menu_gisfire)
+        # Create Lightnings menu
+        self._menu = QMenu(self.tr(u'Wildfire Spread Simulator'), self._menu_gisfire)
+        self._menu_gisfire.addMenu(self._menu)
+        # Setup the toolbar for lightnings plugin
+        self._toolbar = self.iface.addToolBar(u'GisFIRE Wildfire Spread Simulator')
+        self._toolbar.setObjectName(u'GisFIRE Wildfire Spread Simulator')
+        #setup the GisFire pane
+        self._dockwidget = None
 
-        icon_path = ':/plugins/gis_fire_spread_simulator/icon.png'
-        self.add_action(
-            icon_path,
-            text=self.tr(u'GisFIRE'),
-            callback=self.run,
-            parent=self.iface.mainWindow())
+        # Add toolbar buttons
+        self._addToolbarActions()
+        # Add menu entries
+        self._addMenuActions()
+        # Create relations with existing menus and buttons
+        self._addRelations()
 
     #--------------------------------------------------------------------------
-
-    def onClosePlugin(self):
-        """Cleanup necessary items here when plugin dockwidget is closed"""
-
-        #print "** CLOSING GisFIRESpreadSimulator"
-
-        # disconnects
-        self.dockwidget.closingPlugin.disconnect(self.onClosePlugin)
-
-        # remove this statement if dockwidget is to remain
-        # for reuse if plugin is reopened
-        # Commented next statement since it causes QGIS crashe
-        # when closing the docked window:
-        # self.dockwidget = None
-
-        self.pluginIsActive = False
-
 
     def unload(self):
         """Removes the plugin menu item and icon from QGIS GUI."""
-
-        #print "** UNLOAD GisFIRESpreadSimulator"
-
-        for action in self.actions:
-            self.iface.removePluginMenu(
-                self.tr(u'&GisFIRE Wildfire Spread Simulator'),
-                action)
+        # Remove toolbar items
+        for action in self._toolbarActions.values():
+            action.triggered.disconnect()
             self.iface.removeToolBarIcon(action)
-        # remove the toolbar
-        del self.toolbar
+            action.deleteLater()
+        # Remove toolbar
+        if not(self._toolbar is None):
+            self._toolbar.deleteLater()
+        # Remove menu items
+        for action in self._menuActions.values():
+            action.triggered.disconnect()
+            self._menu.removeAction(action)
+            action.deleteLater()
+        # Remove menu
+        if not(self._menu is None):
+            self._menu.deleteLater()
+        # Remove dockwidget
+        if self._dockwidget != None:
+            self._dockwidget.hide()
+            self._dockwidget.deleteLater()
+        # Remove the menu_gisfire only if I'm the only GisFIRE module installed
+        count = 0
+        for name in active_plugins:
+            if name.startswith('GisFIRE-'):
+                count += 1
+        if count == 1:
+            if not(self._menu_gisfire is None):
+                self.iface.mainWindow().menuBar().removeAction(self._menu_gisfire.menuAction())
+                self._menu_gisfire.menuAction().deleteLater()
+                self._menu_gisfire.deleteLater()
 
     #--------------------------------------------------------------------------
-
-    def run(self):
-        """Run method that loads and starts the plugin"""
-
-        if not self.pluginIsActive:
-            self.pluginIsActive = True
-
-            #print "** STARTING GisFIRESpreadSimulator"
-
-            # dockwidget may not exist if:
-            #    first run of plugin
-            #    removed on close (see self.onClosePlugin method)
-            if self.dockwidget == None:
-                # Create the dockwidget (after translation) and keep reference
-                self.dockwidget = GisFIRESpreadSimulatorDockWidget()
-
-            # connect to provide cleanup on closing of dockwidget
-            self.dockwidget.closingPlugin.connect(self.onClosePlugin)
-
-            # show the dockwidget
-            # TODO: fix to allow choice of dock location
-            self.iface.addDockWidget(Qt.RightDockWidgetArea, self.dockwidget)
-            self.dockwidget.show()
