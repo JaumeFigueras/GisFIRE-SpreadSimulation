aadf3cfc6616747e86d77198d1059cad528d7278
JaumeFigueras <jaume.figueras@upc.edu>
2022-02-25 11:43:54 +0100

    added translation strings for catalan

---
+++ src/gisfire_spread_simulation/i18n/gisfire_spread_simulation_ca.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ca">
+<context>
+    <name>GisFIRESpreadSimulation</name>
+    <message>
+        <location filename="../gisfire_spread_simulation.py" line="99"/>
+        <source>Setup GisFIRE Spread Simulation</source>
+        <translation>Configuració de GisFIRE Spread Simulation</translation>
+    </message>
+    <message>
+        <location filename="../gisfire_spread_simulation.py" line="162"/>
+        <source>Setup</source>
+        <translation>Configuració</translation>
+    </message>
+    <message>
+        <location filename="../gisfire_spread_simulation.py" line="206"/>
+        <source>Gis&amp;FIRE</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gisfire_spread_simulation.py" line="221"/>
+        <source>Spread Simulation</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
